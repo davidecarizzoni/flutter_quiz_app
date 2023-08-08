6acb983e84b95d884665a85ea5138a4a51129033
davidecarizzoni <davide.carizzoni@runelab.it>
2023-08-08 14:41:05 +0200

    feat(quiz): stateful

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
 
+class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
